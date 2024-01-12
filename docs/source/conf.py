fc573d06c9a7651b665e008e85a2565ac0a303b7
MelleStarke <mellestarke@gmail.com>
2024-01-12 14:19:01 +0100

    Change font from Raleway to Roboto

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ html_theme_options = {
     "palette": { "scheme":  "slate",
                  "primary": "light-green",
                  "accent":  "lime" },
-    "font": {"text": "Raleway",
+    "font": {"text": "Roboto",
              "code": "Roboto Mono"}
 }
 
