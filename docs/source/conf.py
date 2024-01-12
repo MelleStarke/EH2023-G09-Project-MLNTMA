e59d3eac4ecfb0d399374065f4bb186937739358
MelleStarke <mellestarke@gmail.com>
2024-01-12 14:41:41 +0100

    Bold Raleway attempt 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ html_theme_options = {
     "palette": { "scheme":  "slate",
                  "primary": "light-green",
                  "accent":  "lime" },
-    "font": {"text": "'https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap'",
+    "font": {"text": "Raleway:wght@500",
              "code": "Roboto Mono"}
 }
 
